ad07c1db9712cea33ecd68d8d7619900859db8a2
NicolasProject <nicodavid97@yahoo.fr>
2021-03-17 21:21:47 +0100

    Design du menu à peu près fait

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
@@ -9,20 +9,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  Color menuColor = const Color(0xFF3A6133);
+  Color menuColor2 = const Color.fromRGBO(91,149,81,0.65);
+  Color text_menu_color = const Color.fromRGBO(184,225,234,1);
+
   @override
   Widget build(BuildContext context) {
     var mediaQueryData = MediaQuery.of(context);
+
     final width = mediaQueryData.size.width;
+    final height = mediaQueryData.size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+
+        child: new Container(
+          decoration: decorationBackground(),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -30,71 +34,153 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             new Container(
-              height: 100,
+              height: height/9,
               width: width/5,
-              color: Colors.green,
-              child: new Center(
-                child: new Text('Profil',
-                  style: new TextStyle(
-                    fontSize: 20
-                  ),
 
+              decoration: decorationMenu(),
+              child: new Center(
+                child: Stack(
+                  children: <Widget>[
+                    Text(
+                      'Profil',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 5
+                            ..color = Colors.black12,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Profil',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          color: text_menu_color,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ),
+
+            ),
+
+            new Container(
+              height: height/9,
+              width: width/5,
+
+              decoration: decorationMenu(),
+              child: new Center(
+                child: Stack(
+                  children: <Widget>[
+                    Text(
+                      'Quiz',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 5
+                            ..color = Colors.black12,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Quiz',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          color: text_menu_color,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
 
               ),
             ),
             new Container(
-                height: 100,
+                height: height/9,
                 width: width/5,
-                color: Colors.green,
-                child: new Center(
-                  child: new Text('Quiz',
-                    style: new TextStyle(
-                        fontSize: 20
-                    ),
 
+                decoration: decorationMenu(),
+                child: new Center(
+
+                  child: new Image.asset(
+                    'img/planete_menu.png',
+                    fit: BoxFit.contain),
+                  )
+
+                ),
+
+            new Container(
+                height: height/9,
+                width: width/5,
+
+                decoration: decorationMenu(),
+                child: new Center(
+                  child: Stack(
+                    children: <Widget>[
+                      Text(
+                        'Défis',
+                        style: GoogleFonts.quicksand(
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 5
+                              ..color = Colors.black12,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Défis',
+                        style: GoogleFonts.quicksand(
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                            color: text_menu_color,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
 
                 ),
             ),
             new Container(
-                height: 100,
+                height: height/9,
                 width: width/5,
-                color: Colors.green,
+
+                decoration: decorationMenu(),
                 child: new Center(
-                  child: new Text('Home',
-                    style: new TextStyle(
-                        fontSize: 20
-                    ),
-
-                  ),
-
-                ),
-            ),
-            new Container(
-                height: 100,
-                width: width/5,
-                color: Colors.green,
-                child: new Center(
-                  child: new Text('Défis',
-                    style: new TextStyle(
-                        fontSize: 20
-                    ),
-
-                  ),
-
-                ),
-            ),
-            new Container(
-                height: 100,
-                width: width/5,
-                color: Colors.green,
-                child: new Center(
-                  child: new Text('Carte',
-                    style: new TextStyle(
-                        fontSize: 20
-                    ),
-
+                  child: Stack(
+                    children: <Widget>[
+                      Text(
+                        'Carte',
+                        style: GoogleFonts.quicksand(
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 5
+                              ..color = Colors.black12,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Carte',
+                        style: GoogleFonts.quicksand(
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                            color: text_menu_color,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
 
                 ),
@@ -103,5 +189,37 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+  BoxDecoration decorationMenu(){
+
+    return BoxDecoration(
+      border: Border.all(),
+
+      gradient: LinearGradient(
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+        colors: <Color>[
+          menuColor,
+          menuColor2,
+        ],
+
+      ),
+    );
+
+
+    }
+  BoxDecoration decorationBackground(){
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.bottomLeft,
+        end: Alignment.topRight,
+        colors: <Color>[
+          Color.fromRGBO(36, 36, 36, 1),
+          Color.fromRGBO(36, 36, 36, 0.66),
+        ],
+
+      ),
+    );
+
   }
 }
